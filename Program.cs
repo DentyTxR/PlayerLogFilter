63fefaa4ca1a5db5a7b6a53d6e8a772359a7368e
Denty <dentymods.axuka@gmail.com>
2024-09-08 03:51:10 -0400

    finally figured out publishing, also added very simple error logging (need to make it better, work on the exe itself, icon, cert, single file,etc)

--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
 ﻿using Avalonia;
 using ConfigFactory;
 using System;
+using System.IO;
 
 namespace PlayerLogFilter;
 
 class Program
 {
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            // Log the exception to a file for further analysis
+            File.WriteAllText("startup_error_log.txt", ex.ToString());
+            throw; // Re-throw the exception after logging it
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
